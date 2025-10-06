829e807f6ad1057ca5f7b111353d5c4ceaca88d1
c1nn3r <stevemegr@gmail.com>
2025-10-06 09:13:29 -0700

    edit

--- client/src/components/Portfolio.tsx
+++ client/src/components/Portfolio.tsx
@@ -50,7 +50,7 @@ const portfolioItems: PortfolioItem[] = [
     caption: "Party Prince - Better", 
     genre: "RnB",
     soundcloudUrl: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2183308775&color=%23312e29&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
-"
+
   },
 ];
 
